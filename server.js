04527b5020904c0f381899136d82f024779c4f9b
Tashfeen Mustafa Choudhury <tashfeenc1234@gmail.com>
2016-12-13 22:42:31 +0600

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 var express = require('express')
 var app = express()
+var path = require('path')
 var routes = require('./api/index.js')
-
 
 var port = process.env.PORT || 8080
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+app.use(express.static(path.resolve(__dirname, 'client')))
+
+app.use(function(request, response, next) {
+    response.header("Access-Control-Allow-Origin", "*")
+    response.header("Access-Control-Allow-Methods", "GET")
+    response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
     next();
-  });
+});
 
 routes(app)
 
